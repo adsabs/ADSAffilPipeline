220caadf1d1bdda8755d1de658153f3db9a41558
Matthew Templeton <seasidesparrow@users.noreply.github.com>
2017-10-18 08:46:30 -0400

    Add files via upload
    Final version, needs unittests.

--- affil_match.py
+++ affil_match.py
@@ -4,6 +4,7 @@
 #
 # beta testing for the production code
 #----------------------------------------------------------------------------
+import warnings
 
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
@@ -25,7 +26,6 @@ def read_data(lf,colnames):
     else:
         df=pd.read_csv(f,sep='\t',names=colnames)
         f.close()
-    print("Done read_data: %s"%lf)
     return(df)
 
 
@@ -52,7 +52,6 @@ def learn_dictionary(df):
     cvf=tft.fit_transform(ac)
     clf=SGDClassifier(loss='log', penalty='elasticnet',
                       alpha=1.e-4).fit(cvf,df.index)
-    print("Done learn_dictionary.")
     return(cv,tft,clf,alist)
 
 
@@ -60,8 +59,8 @@ def learn_dictionary(df):
 def match_entries(dict_frame,match_frame,cv,tft,clf,colnames):
 
 # This is where the transforms created in learn_dictionary are applied
-# to the data being matched.
-#
+# to the data being matched, and the best guesses are written to
+# match_frame.
 
     match_namelist=column_to_list(match_frame,'Affil')
 
@@ -70,35 +69,24 @@ def match_entries(dict_frame,match_frame,cv,tft,clf,colnames):
     predicted=clf.predict(ntf)
     probs=clf.predict_proba(ntf)
 
+
     match_aflist=[]
     match_afscore=[]
-    match_dict_nwords=[]
-    match_dict_nuniq=[]
-    match_score_diff=[]
-    nids=len(dict_frame)
-    minprob=1./float(nids)
+
     for p,ip in zip(probs,predicted):
-        p.sort()
-        psig=p.std()
-        difcrit=(psig*3.0)
         match_aflist.append(dict_frame.Affcode[ip])
         match_afscore.append(p.max())
-        match_dict_nwords.append(len(dict_frame.Affil[ip].split()))
-        match_dict_nuniq.append(len(set(dict_frame.Affil[ip].split())))
-        match_score_diff.append((p[-1]-p[-2])-difcrit)
 
     match_frame['Affcodes']=match_aflist
     match_frame['Affscore']=match_afscore
-    match_frame['Dnwords']=match_dict_nwords
-    match_frame['Dnuniq']=match_dict_nuniq
-    match_frame['ScoreD']=match_score_diff
+
     return match_frame
     
 
 
 def parents_children():
-    infile='ids_canonical_MT_2017Oct10.tsv'
-    f=open(infile,'rU')
+    infile='config/MT_2017Oct16_parent_child_facet_inst.tsv'
+    f=open(infile,encoding='ascii',errors='replace')
 
     children=dict([])
     parents=dict()
@@ -106,17 +94,17 @@ def parents_children():
     clist=[]
 
     for l in f.readlines():
-        (p,c,cn)=l.split('\t')
+        (p,c,mcn,cn)=l.split('\t')
         clist.append(c)
-        if len(str(p)) > 0:
+        if len(p) > 0:
             try:
-                children[str(p)]
+                children[p]
             except KeyError:
-                children[str(p)]=[str(c)]
+                children[p]=[c]
             else:
-                children[str(p)].append(str(c))
-            parents[str(c)]=str(p)
-        canonical[str(c)]=cn
+                children[p].append(c)
+            parents[c]=p
+        canonical[c]=cn
     f.close()
 
     return children,parents,canonical
@@ -131,28 +119,28 @@ def get_parent(affil,parents):
 
 
 def print_output(prob_min,match_frame):
-    affil_string=match_frame['Affil'].tolist()
-    bibcode_string=match_frame['bibcode'].tolist()
-    sequence_string=match_frame['sequence'].tolist()
-    test_answers=match_frame['Affcodes'].tolist()
+    bibcode_string=column_to_list(match_frame,'bibcode')
+    sequence_string=column_to_list(match_frame,'sequence')
+    test_answers=column_to_list(match_frame,'Affcodes')
     test_scores=match_frame['Affscore'].tolist()
-    test_nword=match_frame['Dnwords'].tolist()
-    test_nuniq=match_frame['Dnuniq'].tolist()
-    test_scored=match_frame['ScoreD'].tolist()
-    outfile=open('answers.txt','w')
+    matched_affils=open('matched.txt','w')
+    unmatched_affils=open('unmatched.txt','w')
     (children,parents,canonical)=parents_children()
-    for a,ta,bib,seq,ts in zip(affil_string,test_answers,bibcode_string,sequence_string,test_scores):
+    for ta,bib,seq,ts in zip(test_answers,bibcode_string,sequence_string,test_scores):
         try:
             canonical[ta]
         except KeyError:
             b="ERROR:Bad Key!"
+            parent="ERROR:Bad Key!"
         else:
             b=canonical[ta].strip()
+            parent=parents_children(ta)
         if (ts >= 2.*prob_min):
-            outfile.write("YES\t%s\t%s\t%s\t%s\t%s\t%f16.10\n"%(a,ta,b,bib,seq,ts))
+            matched_affils.write("%s\t%s\t%s\t%s\t%s\t%f16.10\n"%(ta,parent,b,bib,seq,ts))
         else:
-            outfile.write("NO\t%s\t%s\t%s\t%s\t%s\t%f16.10\n"%(a,ta,b,bib,seq,ts))
-    outfile.close()
+            unmatched_affils.write("%s\t%s\t%s\t%s\t%s\t%f16.10\n"%(ta,parent,b,bib,seq,ts))
+    matched_affils.close()
+    unmatched_affils.close()
     return
 
 
@@ -160,21 +148,22 @@ def print_output(prob_min,match_frame):
 def get_options():
     import argparse
     parser=argparse.ArgumentParser(description='Affil matching w/sklearn')
-    parser.add_argument('infile',type=str,nargs=1,help='file name of input'
-                        +' dictionary')
     parser.add_argument('testfile',type=str,nargs=1,help='file name of'
                         +' data to be tested')
     args=parser.parse_args()
-    return args.infile[0],args.testfile[0]
+    return args.testfile[0]
 
 
 
 def main():
+#   because sklearn is throwing an annoying FutureWarning in python3
+    warnings.filterwarnings("ignore", category=FutureWarning)
+
 #   get user inputs for filenames
-    (learn_file,target_file)=get_options()
+    target_file=get_options()
 
 #   read the dictionary data
-    dict_frame=read_data(learn_file,['Affcode','Affil'])
+    dict_frame=read_data('config/dictionary.dat',['Affcode','Affil'])
 
 #   transform dictionary using sklearn
     (cvec,transf,cveclfitr,affil_list)=learn_dictionary(dict_frame)
@@ -188,9 +177,6 @@ def main():
     print_output((1./len(dict_frame)),match_entries(dict_frame,match_frame,cvec,transf,cveclfitr,match_columns))
 
 
-#   END
-
 
 if __name__ == '__main__':
     main()
-    print("Done!")
