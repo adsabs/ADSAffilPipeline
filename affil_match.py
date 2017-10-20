4a6ee3a71aed4dc92861e081c165abc5b73651c2
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2017-10-20 13:02:33 -0400

    modified:   affil_match.py  	new file:   config/config.py  	new file:   config/dictionary.dat  	new file:   config/parent_child_facet_inst.tsv  	deleted:    old/am_0.1.py  	deleted:    old/am_0.15.py  	deleted:    old/am_0.2.py  	deleted:    old/am_0.5.py  	deleted:    old/am_0.9.py

--- affil_match.py
+++ affil_match.py
@@ -4,7 +4,6 @@
 #
 # beta testing for the production code
 #----------------------------------------------------------------------------
-import warnings
 
 from config.config import *
 import warnings
@@ -54,13 +53,8 @@ def learn_dictionary(df):
     ac=cv.fit_transform(alist)
     tft=TfidfTransformer()
     cvf=tft.fit_transform(ac)
-<<<<<<< HEAD
     clf=SGDClassifier(loss=SGDC_PARAM_LOSS, penalty=SGDC_PARAM_PENALTY,
                       alpha=SGDC_PARAM_ALPHA).fit(cvf,df.index)
-=======
-    clf=SGDClassifier(loss='log', penalty='elasticnet',
-                      alpha=1.e-4).fit(cvf,df.index)
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
     return(cv,tft,clf,alist)
 
 
@@ -83,12 +77,8 @@ def match_entries(dict_frame,match_frame,cv,tft,clf,colnames):
     match_afscore=[]
 
     for p,ip in zip(probs,predicted):
-<<<<<<< HEAD
 #       match_aflist.append(dict_frame.Affcode[ip])
         match_aflist.append(dict_frame[DICT_COL_CODE][ip])
-=======
-        match_aflist.append(dict_frame.Affcode[ip])
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
         match_afscore.append(p.max())
 
     match_frame['Affcodes']=match_aflist
@@ -98,12 +88,7 @@ def match_entries(dict_frame,match_frame,cv,tft,clf,colnames):
     
 
 
-<<<<<<< HEAD
 def parents_children(infile):
-=======
-def parents_children():
-    infile='config/MT_2017Oct16_parent_child_facet_inst.tsv'
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
     f=open(infile,encoding='ascii',errors='replace')
 
     children=dict([])
@@ -137,22 +122,12 @@ def get_parent(affil,parents):
 
 
 def print_output(prob_min,match_frame):
-<<<<<<< HEAD
     bibcode_string=column_to_list(match_frame,MATCH_COL_BIB)
     sequence_string=column_to_list(match_frame,MATCH_COL_AISQ)
     test_answers=column_to_list(match_frame,'Affcodes')
     test_scores=match_frame['Affscore'].tolist()
     matched_affils=open(OUTPUT_FILE,'w')
     (children,parents,canonical)=parents_children(PC_INFILE)
-=======
-    bibcode_string=column_to_list(match_frame,'bibcode')
-    sequence_string=column_to_list(match_frame,'sequence')
-    test_answers=column_to_list(match_frame,'Affcodes')
-    test_scores=match_frame['Affscore'].tolist()
-    matched_affils=open('matched.txt','w')
-    unmatched_affils=open('unmatched.txt','w')
-    (children,parents,canonical)=parents_children()
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
     for ta,bib,seq,ts in zip(test_answers,bibcode_string,sequence_string,test_scores):
         try:
             canonical[ta]
@@ -161,20 +136,10 @@ def print_output(prob_min,match_frame):
             parent="ERROR:Bad Key!"
         else:
             b=canonical[ta].strip()
-<<<<<<< HEAD
             parent=get_parent(ta,parents)
         ts=int(100*ts/prob_min)/100.
         matched_affils.write("%s\t%s\t%s\t%s\t%s\n"%(ta,parent,bib,seq,ts))
     matched_affils.close()
-=======
-            parent=parents_children(ta)
-        if (ts >= 2.*prob_min):
-            matched_affils.write("%s\t%s\t%s\t%s\t%s\t%f16.10\n"%(ta,parent,b,bib,seq,ts))
-        else:
-            unmatched_affils.write("%s\t%s\t%s\t%s\t%s\t%f16.10\n"%(ta,parent,b,bib,seq,ts))
-    matched_affils.close()
-    unmatched_affils.close()
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
     return
 
 
@@ -190,10 +155,7 @@ def get_options():
 
 
 def main():
-<<<<<<< HEAD
 
-=======
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
 #   because sklearn is throwing an annoying FutureWarning in python3
     warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -201,11 +163,7 @@ def main():
     target_file=get_options()
 
 #   read the dictionary data
-<<<<<<< HEAD
     dict_frame=read_data(DICT_INFILE,DICT_COLS)
-=======
-    dict_frame=read_data('config/dictionary.dat',['Affcode','Affil'])
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
 
 #   transform dictionary using sklearn
     (cvec,transf,cveclfitr,affil_list)=learn_dictionary(dict_frame)
@@ -217,10 +175,7 @@ def main():
 #   classify and output
     print_output((1./len(dict_frame)),match_entries(dict_frame,match_frame,cvec,transf,cveclfitr,MATCH_COLS))
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 220caadf1d1bdda8755d1de658153f3db9a41558
 
 if __name__ == '__main__':
     main()
